6d9d985958021c10472b0118098b492f1324a341
Sam Joseph <seasam@gmail.com>
2020-11-10 12:49:49 +0530

    FIrst commit

---
+++ src/Test1.java
@@ -0,0 +1,11 @@
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("This is what happens in the base project");
+		System.out.println("This is what happens in the base project111");
+	}
+
+}
